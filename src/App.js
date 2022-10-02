200b6d69219433fc16fc4d4e5a771c2ff2e296a9
rokib2000 <rokib0w1@gmail.com>
2022-10-02 14:29:28 +0600

    change file location and update

--- src/App.js
+++ src/App.js
@@ -2,24 +2,12 @@ import "./App.css";
 import Activities from "./components/Activities/Activities";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Container } from "react-bootstrap";
-import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
-import SideBar from "./components/SideBar/SideBar";
-import QueAns from "./components/QueAns/QueAns";
 
 function App() {
   return (
     <div>
       <Container>
-        <Row>
-          <Col xs={{ span: 12, order: 2 }} md={{ span: 8, order: 1 }} className="bg-light">
-            <Activities></Activities>
-            <QueAns></QueAns>
-          </Col>
-          <Col xs={{ span: 12, order: 1 }} md={{ span: 4, order: 2 }}>
-            <SideBar></SideBar>
-          </Col>
-        </Row>
+        <Activities></Activities>
       </Container>
     </div>
   );
--- src/components/Activities/Activities.js
+++ src/components/Activities/Activities.js
@@ -1,6 +1,9 @@
 import React, { useEffect, useState } from "react";
 import Row from "react-bootstrap/Row";
 import Activity from "../Activity/Activity";
+import Col from "react-bootstrap/Col";
+import QueAns from "../QueAns/QueAns";
+import SideBar from "../SideBar/SideBar";
 
 const Activities = () => {
   const [activities, setActivities] = useState([]);
@@ -16,18 +19,27 @@ const Activities = () => {
     // console.log(activity);
     const newList = [...list, activity];
     setList(newList);
+    console.log(newList);
   };
 
   return (
     <div>
-      <h2 className="my-4">Today Activities</h2>
-      <div>
-        <Row xs={1} md={2} lg={3} className="g-4">
-          {activities.map((activity) => (
-            <Activity key={activity.id} activity={activity} handleAddToList={handleAddToList}></Activity>
-          ))}
-        </Row>
-      </div>
+      <Row>
+        <Col xs={{ span: 12, order: 2 }} md={{ span: 8, order: 1 }} className="bg-light">
+          <h2 className="my-4">Today Activities</h2>
+          <div>
+            <Row xs={1} md={2} lg={3} className="g-4">
+              {activities.map((activity) => (
+                <Activity key={activity.id} activity={activity} handleAddToList={handleAddToList}></Activity>
+              ))}
+            </Row>
+          </div>
+          <QueAns></QueAns>
+        </Col>
+        <Col xs={{ span: 12, order: 1 }} md={{ span: 4, order: 2 }}>
+          <SideBar></SideBar>
+        </Col>
+      </Row>
     </div>
   );
 };
